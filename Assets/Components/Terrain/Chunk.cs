3b7dea73120a03ae106ac8dc19eef68944bc891b
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 15:46:10 -0700

    initial unity porject commit

---
+++ Assets/Components/Terrain/Chunk.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Antymology.Terrain
+{
+    public class Chunk : MonoBehaviour
+    {
+        
+    }
+}
