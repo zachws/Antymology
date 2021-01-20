87c543c8ff649a079211f5ba53c5391463b01614
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 16:35:28 -0700

    creation of generate mesh method

--- Assets/Components/Terrain/Chunk.cs
+++ Assets/Components/Terrain/Chunk.cs
@@ -67,24 +67,61 @@ namespace Antymology.Terrain
         /// <param name="localXCoordinate">The local x coordinate of the desired block.</param>
         /// <param name="localYCoordinate">The local y coordinate of the desired block.</param>
         /// <param name="localZCoordinate">The local z coordinate of the desired block.</param>
+        /// <exception cref="System.Exception">Invalid local coordinate (either less than 0, or greater than the chunk diameter.</exception>
         /// <returns>The Block with local coordinates from this chunk.</returns>
-        public Block GetBlock(int localXCoordinate, int localYCoordinate, int localZCoordinate)
+        public AbstractBlock GetBlock(int localXCoordinate, int localYCoordinate, int localZCoordinate)
         {
+            // Error check that the local coordinate exists between 0, and the diameter of the chunk
             if
             (
-                localXCoordinate > radius || localXCoordinate < 0 ||
-                localYCoordinate > radius || localYCoordinate < 0 ||
-                localZCoordinate > radius || localZCoordinate < 0
+                localXCoordinate > ConfigurationManager.Instance.Chunk_Diameter || localXCoordinate < 0 ||
+                localYCoordinate > ConfigurationManager.Instance.Chunk_Diameter || localYCoordinate < 0 ||
+                localZCoordinate > ConfigurationManager.Instance.Chunk_Diameter || localZCoordinate < 0
             )
                 throw new Exception(
                     string.Format("Invalid local coordinate. Chunks have a minimum coordinate of 0, and a maximum coordinate of {0}",
-                    WorldManager.Instance.CHUNK_RADIUS)
+                    ConfigurationManager.Instance.Chunk_Diameter)
                 );
 
             throw new NotImplementedException();
         }
 
+        public void GenerateMesh()
+        {
+            for (int x = 0; x < ConfigurationManager.Instance.Chunk_Diameter; x++)
+                for (int z = 0; z < ConfigurationManager.Instance.Chunk_Diameter; z++)
+                    for (int y = 0; y < ConfigurationManager.Instance.Chunk_Diameter; y++)
+                    {
+                        AbstractBlock ours = GetBlock(x, y, z);
+                        Type ourType = ours.GetType();
+                        if (ourType != typeof(AirBlock))
+                        {
+                            if (GetBlock(x, y + 1, z).GetType() == typeof(AirBlock))
+                                FaceYPos(x, y, z, ours);
+
+                            if (GetBlock(x, y - 1, z).GetType() == typeof(AirBlock))
+                                FaceYNeg(x, y, z, ours);
+
+                            if (GetBlock(x + 1, y, z).GetType() == typeof(AirBlock))
+                                FaceXPos(x, y, z, ours);
+
+                            if (GetBlock(x - 1, y, z).GetType() == typeof(AirBlock))
+                                FaceXNeg(x, y, z, ours);
+
+                            if (GetBlock(x, y, z + 1).GetType() == typeof(AirBlock))
+                                FaceZPos(x, y, z, ours);
+
+                            if (GetBlock(x, y, z - 1).GetType() == typeof(AirBlock))
+                                FaceZNeg(x, y, z, ours);
+                        }
+                    }
+        }
+
         #endregion
-    
+
+        #region Helpers
+
+        #endregion
+
     }
 }
