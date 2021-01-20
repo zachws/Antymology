9e8d6e7de92daaa68f38f74250e4021b1aeaf81a
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 16:35:01 -0700

    creation of configuration manager which will be used to load and save world creation variables

---
+++ Assets/Scripts/ConfigurationManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigurationManager : Singleton<ConfigurationManager>
+{
+
+    /// <summary>
+    /// The number of chunks in any dimension of the world.
+    /// </summary>
+    public int World_Diameter = 16;
+
+    /// <summary>
+    /// The number of blocks in any dimension of a chunk.
+    /// </summary>
+    public int Chunk_Diameter = 8;
+}
