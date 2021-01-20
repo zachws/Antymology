9d42125e9753868c19348737fa5855f3bb71c079
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 16:35:15 -0700

    abstraction of block types

---
+++ Assets/Components/Terrain/Blocks/AbstractBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractBlock
+{
+    /// <summary>
+    /// The woorld x coordinate of this block
+    /// </summary>
+    public int worldXCoordinate;
+
+    /// <summary>
+    /// The world y coordinate of this block
+    /// </summary>
+    public int worldYCoordinate;
+
+    /// <summary>
+    /// The world z coordinate of this block
+    /// </summary>
+    public int worldZCoordinate;
+}
---
+++ Assets/Components/Terrain/Blocks/AirBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Antymology.Terrain
+{
+    /// <summary>
+    /// The air type of block. Contains the internal data representing phermones in the air.
+    /// </summary>
+    public class AirBlock : AbstractBlock
+    {
+
+        #region Fields
+
+        /// <summary>
+        /// A dictionary representing the phermone deposits in the air. Each type of phermone gets it's own byte key, and each phermone type has a concentration.
+        /// </summary>
+        private Dictionary<byte, double> phermoneDeposits;
+
+        #endregion
+
+    }
+}
