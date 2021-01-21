2fa80f2ab6ccf93c91d693796db88485df292a35
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 21:19:54 -0700

    adding mulch block

---
+++ Assets/Components/Terrain/Blocks/MulchBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Antymology.Terrain
+{
+    /// <summary>
+    /// A block of mulch. Ants can use this resource to replenish their hunger.
+    /// </summary>
+    public class MulchBlock : AbstractBlock
+    {
+
+        /// <summary>
+        /// The tile at the 0, 1, position in the tilemap.
+        /// </summary>
+        public override Vector2 tileMapCoordinate => new Vector2(0, 1);
+
+        /// <summary>
+        /// mulch is a visible block.
+        /// </summary>
+        public override bool isVisible => true;
+    }
+}
