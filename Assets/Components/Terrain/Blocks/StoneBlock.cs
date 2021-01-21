424dcd0195d7168af3d09aa06017ea6073b330ee
Cooper Davies <cooper.davies@vizworx.com>
2021-01-20 21:20:01 -0700

    adding stone block

---
+++ Assets/Components/Terrain/Blocks/StoneBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Antymology.Terrain
+{
+    /// <summary>
+    /// A solid stone block.
+    /// </summary>
+    public class StoneBlock : AbstractBlock
+    {
+
+        /// <summary>
+        /// The tile at the 3, 1, position in the tilemap.
+        /// </summary>
+        public override Vector2 tileMapCoordinate => new Vector2(3, 1);
+
+        /// <summary>
+        /// Stone is a visible block.
+        /// </summary>
+        public override bool isVisible => true;
+    }
+}
