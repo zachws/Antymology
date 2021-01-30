ad10c787bea07e41cfc9eaa3b78d79401648cfe1
Cooper Davies <cooper.davies@vizworx.com>
2021-01-30 00:10:23 -0700

    creation of custom math helper

---
+++ Assets/Helpers/CustomMath.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Antymology.Helpers
+{
+    public static class CustomMath
+    {
+        /// <summary>
+        /// Faster version of Int.Floor 
+        /// </summary>
+        public static int fastfloor(double x)
+        {
+            int xi = (int)x;
+            return x < xi ? xi - 1 : xi;
+        }
+
+        /// <summary>
+        /// Returns the dot product of a vector4 with a vector3.
+        /// </summary>
+        public static double dot(Vector4 g, Vector3 prod)
+        {
+            return dot(g, prod.x, prod.y, prod.z);
+        }
+
+        /// <summary>
+        /// Returns the dot product of a vector4 with an x, y, and z.
+        /// </summary>
+        public static double dot(Vector4 g, double x, double y, double z)
+        {
+            return g.x * x + g.y * y + g.z * z;
+        }
+    }
+}
