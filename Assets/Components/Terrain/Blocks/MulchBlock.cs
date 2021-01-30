945bba2b7c622679322a3ba7ab60734590f0f591
Cooper Davies <cooper.davies@vizworx.com>
2021-01-30 00:20:29 -0700

    mulch block implementation

--- Assets/Components/Terrain/Blocks/MulchBlock.cs
+++ Assets/Components/Terrain/Blocks/MulchBlock.cs
@@ -7,15 +7,30 @@ namespace Antymology.Terrain
     /// </summary>
     public class MulchBlock : AbstractBlock
     {
+        /// <summary>
+        /// Statically held tile map coordinate.
+        /// </summary>
+        static Vector2 _tileMapCoordinate = new Vector2(0, 1);
+
+        /// <summary>
+        /// Statically held is visible.
+        /// </summary>
+        static bool _isVisible = true;
 
         /// <summary>
         /// The tile at the 0, 1, position in the tilemap.
         /// </summary>
-        public override Vector2 tileMapCoordinate => new Vector2(0, 1);
+        public override Vector2 tileMapCoordinate()
+        {
+            return _tileMapCoordinate;
+        }
 
         /// <summary>
         /// mulch is a visible block.
         /// </summary>
-        public override bool isVisible => true;
+        public override bool isVisible()
+        {
+            return _isVisible;
+        }
     }
 }
