c1d8930a1de05b4a5a149f3c77dd25e1c3fc3989
Cooper Davies <cooper.davies@vizworx.com>
2021-02-28 20:37:00 -0700

    finishing initial world generation

--- Assets/Components/Terrain/WorldManager.cs
+++ Assets/Components/Terrain/WorldManager.cs
@@ -12,6 +12,16 @@ namespace Antymology.Terrain
         #region Fields
 
         /// <summary>
+        /// The prefab containing the ant.
+        /// </summary>
+        public GameObject antPrefab;
+
+        /// <summary>
+        /// The material used for eech block.
+        /// </summary>
+        public Material blockMaterial;
+
+        /// <summary>
         /// The raw data of the underlying world structure.
         /// </summary>
         private AbstractBlock[,,] Blocks;
@@ -47,13 +57,13 @@ namespace Antymology.Terrain
             // Initialize a new 3D array of blocks with size of the number of chunks times the size of each chunk
             Blocks = new AbstractBlock[
                 ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter,
-                ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter,
+                ConfigurationManager.Instance.World_Height * ConfigurationManager.Instance.Chunk_Diameter,
                 ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter];
 
             // Initialize a new 3D array of chunks with size of the number of chunks
             Chunks = new Chunk[
                 ConfigurationManager.Instance.World_Diameter,
-                ConfigurationManager.Instance.World_Diameter,
+                ConfigurationManager.Instance.World_Height,
                 ConfigurationManager.Instance.World_Diameter];
         }
 
@@ -75,16 +85,16 @@ namespace Antymology.Terrain
         /// </summary>
         public AbstractBlock GetBlock(int WorldXCoordinate, int WorldYCoordinate, int WorldZCoordinate)
         {
-            if 
+            if
             (
                 WorldXCoordinate < 0 ||
                 WorldYCoordinate < 0 ||
                 WorldZCoordinate < 0 ||
-                WorldXCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter ||
-                WorldYCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter ||
-                WorldZCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter
+                WorldXCoordinate >= ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter ||
+                WorldYCoordinate >= ConfigurationManager.Instance.World_Height * ConfigurationManager.Instance.Chunk_Diameter ||
+                WorldZCoordinate >= ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter
             )
-                throw new IndexOutOfRangeException();
+                return new AirBlock();
 
             return Blocks[WorldXCoordinate, WorldYCoordinate, WorldZCoordinate];
         }
@@ -101,17 +111,17 @@ namespace Antymology.Terrain
                 LocalXCoordinate < 0 ||
                 LocalYCoordinate < 0 ||
                 LocalZCoordinate < 0 ||
-                LocalXCoordinate > ConfigurationManager.Instance.Chunk_Diameter ||
-                LocalYCoordinate > ConfigurationManager.Instance.Chunk_Diameter ||
-                LocalZCoordinate > ConfigurationManager.Instance.Chunk_Diameter ||
+                LocalXCoordinate >= ConfigurationManager.Instance.Chunk_Diameter ||
+                LocalYCoordinate >= ConfigurationManager.Instance.Chunk_Diameter ||
+                LocalZCoordinate >= ConfigurationManager.Instance.Chunk_Diameter ||
                 ChunkXCoordinate < 0 ||
                 ChunkYCoordinate < 0 ||
                 ChunkZCoordinate < 0 ||
-                ChunkXCoordinate > ConfigurationManager.Instance.World_Diameter ||
-                ChunkYCoordinate > ConfigurationManager.Instance.World_Diameter ||
-                ChunkZCoordinate > ConfigurationManager.Instance.World_Diameter 
+                ChunkXCoordinate >= ConfigurationManager.Instance.World_Diameter ||
+                ChunkYCoordinate >= ConfigurationManager.Instance.World_Height ||
+                ChunkZCoordinate >= ConfigurationManager.Instance.World_Diameter
             )
-                throw new IndexOutOfRangeException();
+                return new AirBlock();
 
             return Blocks
             [
@@ -132,7 +142,7 @@ namespace Antymology.Terrain
                 WorldYCoordinate < 0 ||
                 WorldZCoordinate < 0 ||
                 WorldXCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter ||
-                WorldYCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter ||
+                WorldYCoordinate > ConfigurationManager.Instance.World_Height * ConfigurationManager.Instance.Chunk_Diameter ||
                 WorldZCoordinate > ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter
             )
                 throw new IndexOutOfRangeException();
@@ -184,9 +194,77 @@ namespace Antymology.Terrain
             GeneratePreliminaryWorld();
         }
 
+        private void GenerateChunks()
+        {
+            GameObject chunkObg = new GameObject("Chunks");
+
+            for (int x = 0; x < Chunks.GetLength(0); x++)
+                for (int z = 0; z < Chunks.GetLength(2); z++)
+                    for (int y = 0; y < Chunks.GetLength(1); y++)
+                    {
+                        GameObject temp = new GameObject();
+                        temp.transform.parent = chunkObg.transform;
+                        temp.transform.position = new Vector3
+                        (
+                            x * ConfigurationManager.Instance.Chunk_Diameter - 0.5f,
+                            y * ConfigurationManager.Instance.Chunk_Diameter + 0.5f,
+                            z * ConfigurationManager.Instance.Chunk_Diameter - 0.5f
+                        );
+                        Chunk chunkScript = temp.AddComponent<Chunk>();
+                        chunkScript.x = x * ConfigurationManager.Instance.Chunk_Diameter;
+                        chunkScript.y = y * ConfigurationManager.Instance.Chunk_Diameter;
+                        chunkScript.z = z * ConfigurationManager.Instance.Chunk_Diameter;
+                        chunkScript.Init();
+                        temp.GetComponent<Renderer>().material = blockMaterial;
+                        chunkScript.GenerateMesh();
+                        Chunks[x, y, z] = chunkScript;
+                    }
+        }
+
         private void GeneratePreliminaryWorld()
         {
+            for (int x = 0; x < Blocks.GetLength(0); x++)
+                for (int z = 0; z < Blocks.GetLength(2); z++)
+                {
+                    /**
+                     * These numbers have been fine-tuned and tweaked through trial and error.
+                     * Altering these numbers may produce weird looking worlds.
+                     **/
+                    int stoneCeiling = SimplexNoise.GetPerlinNoise(x, 0, z, 10, 3, 1.2) +
+                                       SimplexNoise.GetPerlinNoise(x, 300, z, 20, 4, 0) +
+                                       10;
+                    int grassHeight = SimplexNoise.GetPerlinNoise(x, 100, z, 30, 10, 0);
+                    int foodHeight = SimplexNoise.GetPerlinNoise(x, 200, z, 20, 5, 1.5);
 
+                    for (int y = 0; y < Blocks.GetLength(1); y++)
+                    {
+                        if (y <= stoneCeiling)
+                        {
+                            Blocks[x, y, z] = new StoneBlock();
+                        }
+                        else if (y <= stoneCeiling + grassHeight)
+                        {
+                            Blocks[x, y, z] = new GrassBlock();
+                        }
+                        else if (y <= stoneCeiling + grassHeight + foodHeight)
+                        {
+                            Blocks[x, y, z] = new MulchBlock();
+                        }
+                        else
+                        {
+                            Blocks[x, y, z] = new AirBlock();
+                        }
+                        if
+                        (
+                            x == 0 ||
+                            x >= ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter - 1 ||
+                            z == 0 ||
+                            z >= ConfigurationManager.Instance.World_Diameter * ConfigurationManager.Instance.Chunk_Diameter - 1 ||
+                            y == 0
+                        )
+                            Blocks[x, y, z] = new ContainerBlock();
+                    }
+                }
         }
 
         #endregion
