946b1a7c96a9335d7749a1ed080992a39a037dc8
Cooper Davies <cooper.davies@vizworx.com>
2021-02-28 21:54:46 -0700

    adding new vars for required modifications

--- Assets/Components/Configuration/ConfigurationManager.cs
+++ Assets/Components/Configuration/ConfigurationManager.cs
@@ -26,7 +26,27 @@ public class ConfigurationManager : Singleton<ConfigurationManager>
     public int Chunk_Diameter = 8;
 
     /// <summary>
-    /// How much of the tile map does each tile take up
+    /// How much of the tile map does each tile take up.
     /// </summary>
     public float Tile_Map_Unit_Ratio = 0.25f;
+
+    /// <summary>
+    /// The number of acidic regions on the map.
+    /// </summary>
+    public int Number_Of_Acidic_Regions = 10;
+
+    /// <summary>
+    /// The radius of each acidic region
+    /// </summary>
+    public int Acidic_Region_Radius = 5;
+
+    /// <summary>
+    /// The number of acidic regions on the map.
+    /// </summary>
+    public int Number_Of_Conatiner_Spheres = 5;
+
+    /// <summary>
+    /// The radius of each acidic region
+    /// </summary>
+    public int Conatiner_Sphere_Radius = 20;
 }
